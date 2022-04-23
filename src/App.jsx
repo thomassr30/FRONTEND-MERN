0d320466d224482c7a8d619384ed7632cd6e83ff
thomassr30 <thomassoto94@gmail.com>
2022-04-22 23:19:08 -0400

    Estilos para centrar pages en app

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,15 @@ import {Routes, Route} from 'react-router-dom'
 
 const App = () => {
   return (
-    <Routes>
-      <Route path='/' element={<HomePage/>} />
-      <Route path='/new' element={<PostForm/>} />
-      <Route path='*' element={<NotFoundPage/>} />
-    </Routes>
+    <div className='bg-neutral-900 min-h-screen flex items-center'>
+      <div className='px-10 container m-auto'>
+        <Routes>
+          <Route path='/' element={<HomePage/>} />
+          <Route path='/new' element={<PostForm/>} />
+          <Route path='*' element={<NotFoundPage/>} />
+        </Routes>
+      </div>
+    </div>
   )
 }
 
