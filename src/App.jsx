1ceb30fb4705c3a80907378f6154d4f4b504b2c5
thomassr30 <thomassoto94@gmail.com>
2022-04-23 16:34:00 -0400

    Implementación context en App

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import {HomePage,NotFoundPage,PostForm} from './pages/index'
 import {Routes, Route} from 'react-router-dom'
+import { PostContainer } from './context/postContext'
 
 const App = () => {
   return (
     <div className='bg-neutral-900 min-h-screen flex items-center'>
       <div className='px-10 container m-auto'>
-        <Routes>
-          <Route path='/' element={<HomePage/>} />
-          <Route path='/new' element={<PostForm/>} />
-          <Route path='*' element={<NotFoundPage/>} />
-        </Routes>
+        <PostContainer>
+          <Routes>
+            <Route path='/' element={<HomePage/>} />
+            <Route path='/new' element={<PostForm/>} />
+            <Route path='*' element={<NotFoundPage/>} />
+          </Routes>
+        </PostContainer>
       </div>
     </div>
   )
