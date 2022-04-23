5914dbd2a62befab77e41d0e3e94f76882792458
thomassr30 <thomassoto94@gmail.com>
2022-04-22 23:02:33 -0400

    Importación de pages e implementación de react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import {HomePage,NotFoundPage,PostForm} from './pages/index'
+import {Routes, Route} from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>App</div>
+    <Routes>
+      <Route path='/' element={<HomePage/>} />
+      <Route path='/new' element={<PostForm/>} />
+      <Route path='*' element={<NotFoundPage/>} />
+    </Routes>
   )
 }
 
