2243ef1e799f17f7e85130a64e62cad31d331201
thomassr30 <thomassoto94@gmail.com>
2022-04-24 12:15:40 -0400

    Importación useNavigate y cambio color Card

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import {FaEdit,FaTrashAlt} from 'react-icons/fa'
 import toast from 'react-hot-toast'
 import { usePost } from '../context/postContext'
+import { useNavigate } from 'react-router-dom'
 
 const PostCard = ({post}) => {
 
   const {deletePost} = usePost()
+
+  const navigate = useNavigate()
 
   const handleDelete = (id) => {
     toast((t) => (
@@ -33,7 +36,7 @@ const PostCard = ({post}) => {
   }
 
   return (
-    <div className="card w-96 bg-base-100 shadow-xl">
+    <div className="card w-96 bg-gray-100 shadow-xl">
       <figure className="px-10 pt-10">
         <img src={post.image.url} alt="Shoes" className="rounded-full w-36 h-36 object-cover" />
       </figure>
@@ -41,7 +44,10 @@ const PostCard = ({post}) => {
         <h2 className="card-title">{post.title}</h2>
         <p className='overflow-ellipsis'>{post.description}</p>
         <div className="card-actions">
-          <button className="btn btn-primary">
+          <button 
+            className="btn btn-primary"
+            onClick={() => navigate(`/post/${post._id}`)}
+            >
             <FaEdit className='w-4 h-4'/>
           </button>
 
