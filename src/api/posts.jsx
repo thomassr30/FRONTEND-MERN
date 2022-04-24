c7cb6d6f549e5d939fbd6bb7b694e552166f2b59
thomassr30 <thomassoto94@gmail.com>
2022-04-23 20:47:54 -0400

    Función para crear nueva tarea

--- src/api/posts.jsx
+++ src/api/posts.jsx
@@ -3,3 +3,7 @@ import axios from "axios";
 export const getPostRequest = async () => {
     return await axios.get('http://localhost:4000/posts')
 }
+
+export const createPostRequest = async (post) => {
+    return await axios.post('http://localhost:4000/posts', post)
+}
